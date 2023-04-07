12d1776c15893c156e132123edc329283ec1ab02
Phellipe Oliveira <phellipe_ph1@hotmail.com>
2023-04-07 18:23:46 -0300

    Aulas

---
+++ Aula12/aula12.js
@@ -0,0 +1,43 @@
+//Condições aninhadas
+/*if dentro de outro if
+
+
+else if
+
+if (cond1) {
+    BLOCO 1
+} else
+    if (cond2) {
+
+    }else {
+
+    }
+
+*/
+
+// var idade = 15
+// if (idade < 16){
+//     console.log('Não vota!')
+// } else {
+//     if(idade >= 16 && idade < 18){
+//         console.log('Voto Facultativo')
+//     }
+// }
+
+
+//ONDICAO MULTIPLA
+
+/* COMANDO SWITCH() {
+    case valor1:{
+
+    }break                 --> esse comando é obrigatorio
+    case valor2:{
+
+    }break
+    case valor3:{
+
+    }break
+    default:{
+
+    }break
+*/
